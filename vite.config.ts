4bca763013825c0899c20f1022fc08484cc91b63
Rayhan Seh Fred Touboui <touboui.sehfredrayhan@gmail.com>
2026-06-15 22:53:33 +0200

    fix: correct project name in wrangler.toml and update build output directory

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,23 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [tailwindcss(), vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('/node_modules/three')) return 'three'
+          if (id.includes('/node_modules/@tresjs/core')) return 'tresjs'
+          if (id.includes('/node_modules/gsap')) return 'gsap'
+          if (id.includes('/node_modules/@vueuse/core')) return 'vueuse'
+          if (id.includes('/node_modules/@vueuse/motion')) return 'vueuse'
+        },
+      },
     },
   },
 })
